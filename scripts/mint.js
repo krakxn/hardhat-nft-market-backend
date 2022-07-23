008fd60697d401def28b702e19f3baf3494a95cf
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 16:40:07 +0530

    Update mint.js

--- scripts/mint.js
+++ scripts/mint.js
@@ -8,11 +8,13 @@ async function mintAndList() {
     console.log("Minting NFT...")
     const mintTx = await basicNft.mintNft()
     const mintTxReceipt = await mintTx.wait(1)
+    
     console.log(
         `Minted tokenId ${mintTxReceipt.events[0].args.tokenId.toString()} from contract: ${
             basicNft.address
         }`
     )
+    
     if (network.config.chainId == 31337) {
         // Moralis has a hard time if you move more than 1 block!
         await moveBlocks(2, (sleepAmount = 1000))

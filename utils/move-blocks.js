60d6a5e65e632ac9c70aba8158598b98c679c2d0
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 16:45:26 +0530

    Update move-blocks.js

--- utils/move-blocks.js
+++ utils/move-blocks.js
@@ -6,11 +6,13 @@ function sleep(timeInMs) {
 
 async function moveBlocks(amount, sleepAmount = 0) {
     console.log("Moving blocks...")
+    
     for (let index = 0; index < amount; index++) {
         await network.provider.request({
             method: "evm_mine",
             params: [],
         })
+        
         if (sleepAmount) {
             console.log(`Sleeping for ${sleepAmount}`)
             await sleep(sleepAmount)
